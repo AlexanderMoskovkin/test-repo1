3c9a4caecdffedd2faf060cafeea2d6faf8ca095
AlexanderMoskovkin <alexander.moskovkin@devexpress.com>
2015-09-29 12:19:40 +0300

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,6 @@
 var gulp    = require('gulp');
 var Promise = require('promise');
-
+fail
 gulp.task('test', function () {
     console.log('Testing started...');
     return new Promise(function (resolve) {
