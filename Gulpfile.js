30ae2d367abe66ee156e37f1aef7d94c6bca33bd
Alex Mos <alexander.moskovkin@devexpress.com>
2015-10-09 11:04:23 +0300

    fix hostname

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,7 @@ var Promise = require('promise');
 var os      = require('os');
 
 gulp.task('test', function () {
-    console.log('HOSTNAME:', os.hostname);
+    console.log('HOSTNAME:', os.hostname());
     console.log('Testing started...');
     return new Promise(function (resolve) {
         setTimeout(resolve, 10000);
